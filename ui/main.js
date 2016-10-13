b22a54337fea79c8232fc16a6f392cc2a1773f40
Simhadri Akaash <akaash.chinna@gmail.com>
2016-10-13 17:55:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element= document.getElementById('main-text');
 element.innerHTML='New Value in JS';
+//move the image
+
+var img=document.getElemntById('madi');
+img.onclick=function()
+{
+    img-style.marginright="100px";
+}
