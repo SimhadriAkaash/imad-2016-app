c825596f5c4156c4a4dadc03e3c5c5bb0679730b
Simhadri Akaash <akaash.chinna@gmail.com>
2016-10-13 21:39:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,23 @@ var button=document.getElementById('counter');
 var count=0;
 button.onclick=function()
 {
+    var request = new XMLHttpRequest();
     
-    
- 
- 
- counter=counter+1;
- var span=document.getElementById('count');
+ request.onreadystatechange=function()
+ {
+    if(request.readyState==XMLHttpRequest().DONE)
+    {
+        if(request.status==200)
+        {
+            var counter=request.responseText;
+            var span=document.getElementById('count');
  span.innerHTML=counter.toString();
-}
+        }
+    }
+ }
+     
+ };
+ 
+ request.opem('GET','http://simhadriakaash.imad.hasura-app.io/counter',true);
+ request.send(null);
+};
